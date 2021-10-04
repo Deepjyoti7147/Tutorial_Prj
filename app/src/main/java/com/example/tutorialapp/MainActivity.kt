e17a24f8667f08f2dc42575d0810218c80d5cd8f
Deepjyoti Purkayastha <deepjyoti.college@gmail.com>
2021-10-05 00:48:19 +0530

    First Commit

--- app/src/main/java/com/example/tutorialapp/MainActivity.kt
+++ app/src/main/java/com/example/tutorialapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.tutorialapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var quantity: Int = 0
+    private fun updateValue() {
+        findViewById<TextView>(R.id.qtr).text = quantity.toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        findViewById<Button>(R.id.add).setOnClickListener {
+            quantity++
+            updateValue()
+        }
+        findViewById<Button>(R.id.sub).setOnClickListener {
+            quantity--
+            updateValue()
+        }
+
     }
+
 }
+
