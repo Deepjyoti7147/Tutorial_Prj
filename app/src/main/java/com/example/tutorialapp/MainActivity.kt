99a41b42a3eb1288cff060d7e6a4562c56cea9a0
Deepjyoti Purkayastha <deepjyoti.college@gmail.com>
2021-10-07 20:23:04 +0530

    mvb

--- app/src/main/java/com/example/tutorialapp/MainActivity.kt
+++ app/src/main/java/com/example/tutorialapp/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.example.tutorialapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
 
+<<<<<<< Updated upstream
     private var quantity: Int = 0
     private var price: Int = 0
     private fun updateValue() {
         findViewById<TextView>(R.id.qtr).text = quantity.toString()
         findViewById<TextView>(R.id.prc).text = price.toString()
+=======
+    var quantity: Int = 0
+    var price: Int = 0
+    private fun updateValue() {
+        findViewById<TextView>(R.id.qtr).text = quantity.toString()
+        findViewById<TextView>(R.id.pricevalue).text = price.toString()
+>>>>>>> Stashed changes
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+<<<<<<< Updated upstream
         findViewById<Button>(R.id.add).setOnClickListener {
             quantity++
             price += 3
@@ -42,6 +51,31 @@ class MainActivity : AppCompatActivity() {
                 updateValue()
             }
 
+=======
+
+
+
+        findViewById<Button>(R.id.add).setOnClickListener{
+
+                quantity++
+                price += 3
+                updateValue()
+
+        }
+
+
+        findViewById<Button>(R.id.sub).setOnClickListener {
+            if (quantity == 0)
+            {
+                quantity = 0
+            }
+            else
+            {
+                quantity--
+                price -= 3
+                updateValue()
+            }
+>>>>>>> Stashed changes
         }
 
     }
